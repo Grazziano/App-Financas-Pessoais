1370587a23c3f5c89013961cd790ee662054610c
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-06-28 14:54:09 -0300

    Improving home page

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ButtonMenu, Container, Title } from './styles';
+import Icon from 'react-native-vector-icons/Feather';
+
+import { useNavigation } from '@react-navigation/native';
+
+export default function Header({ title }) {
+  const navigation = useNavigation();
+
+  return (
+    <Container>
+      <ButtonMenu onPress={() => navigation.openDrawer()}>
+        <Icon name="menu" size={35} color="#121212" />
+      </ButtonMenu>
+      {title && <Title>{title}</Title>}
+    </Container>
+  );
+}
