e7bbecf033672887eeae810b6befe505f6a001a3
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-07-01 19:40:55 -0300

    Calendar in portuguese

--- src/components/CalendarModal/index.js
+++ src/components/CalendarModal/index.js
@@ -8,6 +8,10 @@ import {
 import { TouchableWithoutFeedback, View } from 'react-native';
 
 import { Calendar, LocaleConfig } from 'react-native-calendars';
+import { ptBR } from './localeCalendar';
+
+LocaleConfig.locales['pt-br'] = ptBR;
+LocaleConfig.defaultLocale = 'pt-br';
 
 export default function CalendarModal({ setVisible, handleFilter }) {
   const [dateNow, setDateNow] = useState(new Date());
